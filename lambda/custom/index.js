355c263af2e4ee52503f8b34d396c9801baba7cb
Clément Roig <clm.roig@gmail.com>
2018-10-12 16:16:17 +0200

    Rewrite HelloWorldIntent (for test puposes currently)

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -26,6 +26,23 @@ const LaunchRequestHandler = {
   }
 };
 
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === "HelloWorldIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speechText = "Hello World!";
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard("Hello World", speechText)
+      .withSimpleCard(speechText)
+      .getResponse();
+  }
+};
+
 const HelpIntentHandler = {
   canHandle(handlerInput) {
     return (
@@ -101,6 +118,7 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
+    HelloWorldIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
