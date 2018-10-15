88e6291e2d1f4d57eee8a0166be3b422eab7a832
Kevin giordani <giordani.kevin.02@gmail.com>
2018-10-15 23:29:09 +0200

    fix

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Alexa = require("alexa-sdk");
 const config = require("./config"); // Config
 
@@ -8,6 +7,7 @@ const config = require("./config"); // Config
 const welcomeStateHandler = require("./handlers/welcomeHandler");
 const guessMyNumberStateHandler = require("./handlers/guessMyNumberHandler");
 const sendMessageHandler = require("./handlers/sendMessageHandler");
+const singLullabyStateHandler = require("./handlers/singLullabyHandler");
 
 const newSessionHandlers = {
   LaunchRequest: function() {
@@ -25,42 +25,12 @@ exports.handler = function(event, context) {
   //alexa.appId = config.APP_ID;
   // To enable string internationalization (i18n) features, set a resources object.
   //alexa.resources = config.languageString;
-  alexa.registerHandlers(newSessionHandlers, welcomeStateHandler, guessMyNumberStateHandler, sendMessageHandler);
-  alexa.execute();
-};
-=======
-const Alexa = require("alexa-sdk");
-const config = require("./config"); // Config
-
-/**
- * We import handlers as they are separated in different files
- */
-const welcomeStateHandler = require("./handlers/welcomeHandler");
-const guessMyNumberStateHandler = require("./handlers/guessMyNumberHandler");
-const singLullabyStateHandler = require("./handlers/singLullabyHandler");
-
-const newSessionHandlers = {
-  LaunchRequest: function() {
-    this.handler.state = config.WELCOME_STATE;
-    this.emitWithState("Welcome");
-  },
-  Unhandled: function() {
-    this.response.speak("Pas compris").listen("Pas compris");
-    this.emit(":responseReady");
-  }
-};
-
-exports.handler = function(event, context) {
-  const alexa = Alexa.handler(event, context);
-  //alexa.appId = config.APP_ID;
-  // To enable string internationalization (i18n) features, set a resources object.
-  //alexa.resources = config.languageString;
   alexa.registerHandlers(
+    newSessionHandlers,
+    welcomeStateHandler,
     guessMyNumberStateHandler,
-    newSessionHandlers, 
-    singLullabyStateHandler,
-    welcomeStateHandler
+    sendMessageHandler,
+    singLullabyStateHandler
   );
   alexa.execute();
 };
->>>>>>> master
