02b61eb4415e59b95736cefd5f3a3a42725c2815
Clément Roig <clm.roig@gmail.com>
2018-10-16 00:07:13 +0200

    Auto stash before rebase of "origin/master"

--- lambda/custom/handlers/singLullabyHandler.js
+++ lambda/custom/handlers/singLullabyHandler.js
@@ -3,15 +3,17 @@ const config = require("../config");
 const msgH = require("../messages/msgHelper");
 
 const GENERAL_MSG = require("../messages/generalMessages.json");
-const MSG = require("../messages/singLullabyMessages.json");
+const MSG = require('../messages/singLullabyMessages.json');
+
+const LULLABY_URL = "https://s3.amazonaws.com/bibou/invocation.mp3"
 
 // ================================
 
 const singLullabyHandler = Alexa.CreateStateHandler(config.STATES.SING_LULLABY_STATE, {
   StartSinging() {
-    const speechOutput = "La lala lal je chante";
-    this.response.speak(speechOutput).listen(speechOutput);
-    this.emit(":responseReady");
+    const speechOutput = "<audio src='" + LULLABY_URL + "'/>"
+    this.response.speak(speechOutput)
+    this.emit(':responseReady');
   },
   Unhandled() {
     let speechOutput = msgH.pickOne(GENERAL_MSG.errors);
